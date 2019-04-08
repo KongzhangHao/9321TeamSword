04fb38c2c6cc08317de2d215616f4b4545017d64
KongzhangHao <haokongzhang@gmail.com>
2019-04-08 12:04:26 +1000

    clean data, process

--- src/algorithms/svm/svm.py
+++ src/algorithms/svm/svm.py
@@ -1,5 +1,26 @@
 import csv
 
+def is_clean_data(row):
+    if "?" in row:
+        return False
+    return True
+
+
+# [0, 2, 3, 7, 9, 11, 12, 15, 17, 18, 25, 28, 32, 35, 36]
+def preprocess(row):
+    global headers
+    nums = [0]
+    result = []
+    # Cast each term to int
+    row = list(map(lambda x: float(x), row))
+    # Reassign final result
+    if row[0] < 40:
+        result += [1, 0]
+    else:
+        result += [0, 1]
+    nums.append(len(result))
+    headers += ["a1_0", "a1_1"]
+
 def read_csv(path):
     rows = []
     with open(path, newline='') as csvfile:
