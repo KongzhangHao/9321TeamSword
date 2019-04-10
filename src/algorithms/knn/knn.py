d09df96624be15858b51d1a9e06c3555d3213eb2
Xingpeng Z <xingpengzxp@gmail.com>
2019-04-10 21:30:43 +1000

    optimize preprocessing

--- src/algorithms/knn/knn.py
+++ src/algorithms/knn/knn.py
@@ -47,6 +47,57 @@ def pre_process_data(df):
              'a12_1', 'a12_2', 'a12_3', 'a13_3', 'a13_6', 'a13_7', 'a14']]
 
     return df
+def pre_process_data_optimised(df):
+    # normalize continuous value of attributes
+    for column in df:
+        if column == 'a1' or column == 'a4' or column == 'a5' or column == 'a8' or column == 'a10':
+            diff = max(df[column]) - min(df[column])
+            df[column] = (df[column] - min(df[column])) / diff
+
+    # split discrete value of attributes
+    # attribute 3
+    df['a3_1'] = df['a3'].map(lambda x: x == 1)
+    df['a3_2'] = df['a3'].map(lambda x: x == 2)
+    df['a3_3'] = df['a3'].map(lambda x: x == 3)
+    df['a3_4'] = df['a3'].map(lambda x: x == 4)
+    df = df.drop(['a3'], axis=1)
+
+    # attribute 7
+    df['a7_0'] = df['a7'].map(lambda x: x == 0)
+    df['a7_1'] = df['a7'].map(lambda x: x == 1)
+    df['a7_2'] = df['a7'].map(lambda x: x == 2)
+    df = df.drop(['a7'], axis=1)
+
+    # attribute 11
+    df['a11_1'] = df['a11'].map(lambda x: x == 1)
+    df['a11_2'] = df['a11'].map(lambda x: x == 2)
+    df['a11_3'] = df['a11'].map(lambda x: x == 3)
+    df = df.drop(['a11'], axis=1)
+
+    # attribute 12
+    df['a12_0'] = df['a12'].map(lambda x: x == 0)
+    df['a12_1'] = df['a12'].map(lambda x: x == 1)
+    df['a12_2'] = df['a12'].map(lambda x: x == 2)
+    df['a12_3'] = df['a12'].map(lambda x: x == 3)
+    df = df.drop(['a12'], axis=1)
+
+    # attribute 13
+    df['a13_3'] = df['a13'].map(lambda x: x == 3)
+    df['a13_6'] = df['a13'].map(lambda x: x == 6)
+    df['a13_7'] = df['a13'].map(lambda x: x == 7)
+    df = df.drop(['a13'], axis=1)
+
+    # attribute 14
+    df['a14'] = df['a14'].map(lambda x: x != 0)
+
+    df = df.astype(float)
+
+    # re-arrange attributes columns
+    df = df[['a1', 'a2', 'a3_1', 'a3_2', 'a3_3', 'a3_4', 'a4', 'a5','a6',
+             'a7_0', 'a7_1', 'a7_2', 'a8', 'a9', 'a10', 'a11_1', "a11_2", "a11_3", 'a12_0',
+             'a12_1', 'a12_2', 'a12_3', 'a13_3', 'a13_6', 'a13_7', 'a14']]
+
+    return df
 
 
 def pre_process_data_normalisation(df):
